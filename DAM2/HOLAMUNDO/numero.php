30f8f0b4627dd64a8db734c99b307ae0ddeeba1d
jsanchoc <jsanchocalzada@gmail.com>
2025-09-16 18:29:45 +0200

    subida del archivo número

--- DAM2/HOLAMUNDO/numero.php
+++ DAM2/HOLAMUNDO/numero.php
@@ -1,3 +1,12 @@
-<html>
-    <input type="número: ">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Número</title>
+</head>
+<body>
+    <label>Número:</label>
+    <input value="valor">
+</body>
 </html>
